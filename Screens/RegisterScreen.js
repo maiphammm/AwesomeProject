1eec78e49261304b51cc370850766477f2e0d720
maiphammm <ngocmaipham196@gmail.com>
2024-04-20 00:47:59 +1000

    save

--- Screens/RegisterScreen.js
+++ Screens/RegisterScreen.js
@@ -163,7 +163,7 @@ export function RegisterScreen(props) {
   return (
     <View style={styles.container}>
       <View style={styles.TopView}>
-        {<Image style={styles.ImageStyle} source={require('../assets/logoCoffee.png')} /> }
+        {<Image style={styles.ImageStyle} source={require('../assets/logo.png')} /> }
       </View>
 
       <ScrollView style={styles.BottomView}>
